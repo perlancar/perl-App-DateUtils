6ac193e695320e727b2ee734d7f6947de792daad
perlancar (@pc-office) <perlancar@gmail.com>
2015-09-23 11:31:46 +0700

    Initial commit

---
+++ lib/App/DateUtils.pm
@@ -0,0 +1,21 @@
+package App::DateUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: An assortment of date-/time-related CLI utilities
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities related to
+date/time:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<App::datecalc>
+
+=cut
